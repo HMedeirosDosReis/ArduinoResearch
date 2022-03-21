ffa5e41811a02b42a8ee3ada15be2ad6580c9453
Henrique Medeiros Dos Reis <henriquereis20180@gmail.com>
2022-03-21 10:34:10 -0500

    added driving functions to the webAutonomousRobot. Needs testing and check what the next steps are

--- MiniTankRobotV2-keyestudio/webAutonomousRobot/webAutonomousRobot/webAutonomousRobot.ino
+++ MiniTankRobotV2-keyestudio/webAutonomousRobot/webAutonomousRobot/webAutonomousRobot.ino
@@ -10,10 +10,38 @@ WiFiClient client = server.available();
 
 int ledPin = LED_BUILTIN;
 // int ledPin = LED_BUILTIN;2
+#define SCL_Pin A5 // Set clock pin to A5
+#define SDA_Pin A4 // Set data pin to A4
+
+#define ML_Ctrl 13  // define the direction control pin of left motor
+#define ML_PWM 11   // define PWM control pin of left motor
+#define MR_Ctrl 12  // define the direction control pin of right motor
+#define MR_PWM 3    // define PWM control pin of right motor
+#define servoPin 10 // pin of servo
+int pulsewidth;     // save the pulse width value of servo
+
+int trigPin = 5; // ultrasonic trig Pin
+int echoPin = 4; // ultrasonic echo Pin
+long duration, cm;
+int random2;
+int a;
+int a1;
+int a2;
 void setup()
 {
   Serial.begin(9600);
-  pinMode(ledPin, OUTPUT);
+  // motors
+  pinMode(ML_Ctrl, OUTPUT);
+  pinMode(ML_PWM, OUTPUT);
+  pinMode(MR_Ctrl, OUTPUT);
+  pinMode(MR_PWM, OUTPUT);
+  // trig echo
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  // servo
+  pinMode(servoPin, OUTPUT);
+  procedure(90);
+
   while (!Serial)
     ;
 
@@ -144,32 +172,158 @@ void printWEB()
 
         if (currentLine.endsWith("GET /R"))
         {
-          // should go right
+          carRight(); // should go right
         }
         if (currentLine.endsWith("GET /L"))
         {
-          // should go left
+          carLeft(); // should go left
         }
         if (currentLine.endsWith("GET /F"))
         {
-          // should go foward
+          carFront(); // should go foward
         }
         if (currentLine.endsWith("GET /B"))
         {
-          // should go back
+          carBack(); // should go back
         }
-        if (currentLine.endsWith("GET /A"))
+        // needs testing
+        while (currentLine.endsWith("GET /A"))
         {
-          // should turn on autopilot
+          carAuto(); // should turn on autopilot
         }
         if (currentLine.endsWith("GET /O"))
         {
-          // should turn off autopilot
+          carStop(); // should turn off autopilot
         }
       }
     }
     // close the connection:
     client.stop();
     Serial.println("client disconnected");
+  }
+}
+/*servo control---needs work*/
+void procedure(int myangle)
+{
+  for (int i = 0; i <= 50; i = i + (1))
+  {
+    pulsewidth = myangle * 11 + 500;
+    digitalWrite(servoPin, HIGH);
+    delayMicroseconds(pulsewidth);
+    digitalWrite(servoPin, LOW);
+    delay((20 - pulsewidth / 1000));
+  }
+}
+/*drive*/
+void carFront()
+{
+  digitalWrite(MR_Ctrl, LOW);
+  analogWrite(MR_PWM, 200);
+  digitalWrite(ML_Ctrl, LOW);
+  analogWrite(ML_PWM, 200);
+}
+void carBack()
+{
+  digitalWrite(MR_Ctrl, HIGH);
+  analogWrite(MR_PWM, 200);
+  digitalWrite(ML_Ctrl, HIGH);
+  analogWrite(ML_PWM, 200);
+}
+void carLeft()
+{
+  digitalWrite(MR_Ctrl, LOW);
+  analogWrite(MR_PWM, 255);
+  digitalWrite(ML_Ctrl, HIGH);
+  analogWrite(ML_PWM, 255);
+}
+void carRight()
+{
+  digitalWrite(MR_Ctrl, HIGH);
+  analogWrite(MR_PWM, 255);
+  digitalWrite(ML_Ctrl, LOW);
+  analogWrite(ML_PWM, 255);
+}
+void carStop()
+{
+  digitalWrite(MR_Ctrl, LOW);
+  analogWrite(MR_PWM, 0);
+  digitalWrite(ML_Ctrl, LOW);
+  analogWrite(ML_PWM, 0);
+}
+
+// The function to control ultrasonic sensor
+float checkdistance()
+{
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  float distance = pulseIn(echoPin, HIGH) / 58.00; // 58.20, that is, 2*29.1=58.2
+  delay(10);
+  Serial.println(distance);
+  return distance;
+}
+// autopilot
+void carAuto()
+{
+  // Serial.println(distance);
+  random2 = random(1, 100);
+  a = checkdistance(); // assign the front distance detected by ultrasonic sensor to variable a
+
+  if (a < 20) // when the front distance detected is less than 20
+  {
+    carStop();      // robot stops
+    delay(500);     // delay in 500ms
+    procedure(160); // Ultrasonic platform turns left
+    for (int j = 1; j <= 10; j = j + (1))
+    {                       // for statement, the data will be more accurate if ultrasonic sensor detect a few times.
+      a1 = checkdistance(); // assign the left distance detected by ultrasonic sensor to variable a1
+    }
+    delay(300);
+    procedure(20); // Ultrasonic platform turns right
+    for (int k = 1; k <= 10; k = k + (1))
+    {
+      a2 = checkdistance(); // assign the right distance detected by ultrasonic sensor to variable a2
+    }
+
+    if (a1 < 50 || a2 < 50) // robot will turn to the longer distance side when left or right distance is less than 50cm.
+    {
+      if (a1 > a2) // left distance is greater than right side
+      {
+        procedure(90); // Ultrasonic platform turns back to right ahead
+        carLeft();     // robot turns left
+        delay(500);    // turn left for 500ms
+        carFront();    // go front
+      }
+      else
+      {
+        procedure(90);
+        carRight(); // robot turns right
+        delay(500);
+        carFront(); // go front
+      }
+    }
+    else // If both side is greater than or equal to 50cm, turn left or right randomly
+    {
+      if ((long)(random2) % (long)(2) == 0) // When the random number is even
+      {
+        procedure(90);
+        carLeft(); // tank robot turns left
+        delay(500);
+        carFront(); // go front
+      }
+      else
+      {
+        procedure(90);
+        carRight(); // robot turns right
+        delay(500);
+        carFront(); // go front
+      }
+    }
+  }
+  else // If the front distance is greater than or equal to 20cm, robot car will go front
+  {
+    carFront(); // go front
   }
 }
